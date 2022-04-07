2d4063b3f01061c629670197aaa17e8d63cf74d1
94-c <hyeongwoo26@gmail.com>
2022-04-07 22:34:52 +0900

    10. Hibernate CRUD example

--- src/main/java/com/infotech/client/ClientTest.java
+++ src/main/java/com/infotech/client/ClientTest.java
@@ -17,48 +17,61 @@ public class ClientTest {
             System.out.println("MY SQL version is :: ");
             System.out.println(result);*/
 
-            /*Employee employee = getEmployee3();*/
+            deleteEmployeeById(session);
 
-            session.beginTransaction();
-
-            session.persist(getEmployee1());
-
-            Integer id =(Integer)session.save(getEmployee2());
-            System.out.println("Employee is created  with Id::"+id);
-
-
-            session.saveOrUpdate(getEmployee3());
-
-            session.getTransaction().commit();
         } catch (HibernateException e) {
             e.printStackTrace();
         }
     }
 
-    private static Employee getEmployee3(){
+    private static void deleteEmployeeById(Session session) {
+        Employee employee = session.get(Employee.class, 12);
+        if(employee != null){
+            session.beginTransaction();
+
+            session.delete(employee);
+            session.getTransaction().commit();
+        }else{
+            System.out.println("Employee doesn't exist with provideded Id..");
+        }
+    }
+
+    private static void updateEmployeeById(Session session) {
+        Employee employee = session.get(Employee.class, 12);
+        if(employee != null){
+            employee.setSalary(40000.00);
+            session.beginTransaction();
+
+            session.update(employee);
+            session.getTransaction().commit();
+        }else{
+            System.out.println("Employee doesn't exist with provideded Id..");
+        }
+    }
+
+    private static void getEmployeeById(Session session) {
+        Employee employee = session.get(Employee.class, 12);
+        if (employee != null) {
+            System.out.println(employee);
+        }else {
+            System.out.println("Employee doesn't exist with provideded Id.. ");
+        }
+    }
+
+    private static void createEmployee(Session session) {
+        session.beginTransaction();
+        Integer id =(Integer) session.save(getEmployee());
+        System.out.println("Employee is created  with Id::"+id);
+        session.getTransaction().commit();
+    }
+
+    private static Employee getEmployee(){
         Employee employee= new Employee();
-        employee.setEmployeeName("Martin Bingel");
-        employee.setEmail("martin.cs2017@gmail.com");
+        employee.setEmployeeName("hyeongwoo");
+        employee.setEmail("hyeongwoo26@outlook.kr");
         employee.setSalary(80000.00);
         employee.setDoj(new Date());
         return employee;
     }
 
-    private static Employee getEmployee1(){
-        Employee employee= new Employee();
-        employee.setEmployeeName("Frank Bingel");
-        employee.setEmail("frank.cs2017@gmail.com");
-        employee.setSalary(30000.00);
-        employee.setDoj(new Date());
-        return employee;
-    }
-
-    private static Employee getEmployee2(){
-        Employee employee= new Employee();
-        employee.setEmployeeName("Sean Bingel");
-        employee.setEmail("sean.cs2017@gmail.com");
-        employee.setSalary(60000.00);
-        employee.setDoj(new Date());
-        return employee;
-    }
 }
